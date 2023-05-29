6c1b991b1daf42165ea1ed57831255004f22d959
victor-levus <95586250+victor-levus@users.noreply.github.com>
2023-05-29 22:40:27 +0100

    build game screenshots

---
+++ src/components/GameScreenshots.tsx
@@ -0,0 +1,26 @@
+import { Image, SimpleGrid, Spinner } from "@chakra-ui/react";
+import useScreenshots from "../hooks/useScreenShots";
+
+interface Props {
+  gameId: number;
+}
+
+const GameScreenshots = ({ gameId }: Props) => {
+  const { data, error, isLoading } = useScreenshots(gameId);
+
+  console.log(data);
+
+  if (isLoading) return null;
+
+  if (error) throw error;
+
+  return (
+    <SimpleGrid spacing={3} columns={{ base: 1, md: 2 }}>
+      {data?.results.map((f) => (
+        <Image key={f.id} src={f.image} />
+      ))}
+    </SimpleGrid>
+  );
+};
+
+export default GameScreenshots;
